ea0c8455cac59bca21a866fb91428f0d22992837
Kasper Seweryn <github@wvffle.net>
2021-11-30 14:58:39 +0100

    fix: fix 4th exercise

--- 4.js
+++ 4.js
@@ -1,1 +1,1 @@
-const fun = arr => arr.filter((_, i) => !!(i % 2)).map((e, i, a) => e - Math.min(...a)).map((e, i, a) => e / Math.max(...a))
+const fun = arr => console.log(arr.filter((_, i) => !!(i % 2)).map((e, i, a) => e - Math.min(...a)).map((e, i, a) => (e / Math.max(...a)).toFixed(2)).join(' '))
